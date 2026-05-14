76cf0eecbe510c1d287aa6b9fdb71961d7e3e2ed
tobiichi大湿 <2141798809@qq.com>
2026-05-14 15:06:50 +0800

    Update 4导入模块.py

--- 2026Python/2026.5.13(类型注解,函数类型注解,模块,导入模块)/4导入模块.py
+++ 2026Python/2026.5.13(类型注解,函数类型注解,模块,导入模块)/4导入模块.py
@@ -4,7 +4,7 @@
 #   导入形式                             代码样例                            调用方式          调用方式
 # import 模块名                      import random,os                    模块名.功能名  random.randint(10.100)
 # import 模块名 as 别名              import random as rd                 别名.功能名  rd.randint(10,100)
-# from 模块名 import 功能名          import random import randint,choice 功能名       randint(10,100)
+# from 模块名 import 功能名          from random import randint,choice 功能名       randint(10,100)
 # from 模块名 import 功能名 as 别名  from random import randint as rint  别名        rint(10,100)
 # from 模块名 import *              from import random *                功能名      randint(10,100)
 
