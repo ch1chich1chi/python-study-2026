264381307522d1de87e26512cd151430b833dd3c
tobiichi大湿 <2141798809@qq.com>
2026-05-31 17:11:19 +0800

    Update 3ai_partner_1.py

--- 2026Python/2026.5.29(Streamlit基础语法,页面设置,ai_partner)/3ai_partner_1.py
+++ 2026Python/2026.5.29(Streamlit基础语法,页面设置,ai_partner)/3ai_partner_1.py
@@ -50,7 +50,7 @@ if prompt: # 字符串会自动转化为布尔值,如果字符串非空,则为Tr
     st.chat_message("user").write(prompt)
     print("------> 调用AI大模型,提示词:",prompt)
     # 保存用户输入的提示词
-    st.session_state.message.append({"role":"users","content":prompt})
+    st.session_state.message.append({"role":"user","content":prompt})
 
     # 调用AI大模型
     response = client.chat.completions.create(
@@ -68,4 +68,4 @@ if prompt: # 字符串会自动转化为布尔值,如果字符串非空,则为Tr
     print("<------- 大模型返回的结果:",response.choices[0].message.content)
     st.chat_message("assistant").write(response.choices[0].message.content)
     # 保存大模型返回的结果
-    st.session_state.message.append({"role":"system","content":response.choices[0].message.content})
+    st.session_state.message.append({"role":"assistant","content":response.choices[0].message.content})
